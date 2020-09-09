22af7d6fbbe6c5c0ca161672073eabb4ea0da030
Anwesh43 <anweshthecool0@gmail.com>
2020-09-10 02:14:04 +0530

    adding Canvas extension methods

--- birightangledtriview/src/main/java/com/anwesh/uiprojects/birightangledtriview/BiRightAngledTriView.kt
+++ birightangledtriview/src/main/java/com/anwesh/uiprojects/birightangledtriview/BiRightAngledTriView.kt
@@ -31,3 +31,39 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawBiRightAngleTri(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf3 : Float = sf.divideScale(2, parts)
+    val sf4 : Float = sf.divideScale(3, parts)
+    val size : Float = Math.min(w, h) / sizeFactor
+    save()
+    translate(w / 2, h / 2)
+    rotate(rot * sf4)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        if (sf1 > 0f) {
+            drawLine(0f, 0f, -size * sf1, 0f, paint)
+        }
+        if (sf2 > 0f) {
+            drawLine(-size, 0f, -size, -size * sf2, paint)
+        }
+        if (sf3 > 0f) {
+            drawLine(-size, -size, -size + size * sf3, -size + size * sf3, paint)
+        }
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawBRATNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBiRightAngleTri(scale, w, h, paint)
+}
