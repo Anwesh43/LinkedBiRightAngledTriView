6805719002294e9965746e7c815b7f1613f00c48
Anwesh43 <anweshthecool0@gmail.com>
2020-09-10 02:26:38 +0530

    adding Renderer class

--- birightangledtriview/src/main/java/com/anwesh/uiprojects/birightangledtriview/BiRightAngledTriView.kt
+++ birightangledtriview/src/main/java/com/anwesh/uiprojects/birightangledtriview/BiRightAngledTriView.kt
@@ -194,4 +194,27 @@ class BiRightAngledTriView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : BiRightAngledTriView) {
+
+        private val animator : Animator = Animator(view)
+        private val brat : BRATNode = BRATNode(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            brat.draw(canvas, paint)
+            animator.animate {
+                brat.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            brat.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
