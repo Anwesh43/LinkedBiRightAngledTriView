fba6e9c1ce057d273c0c5633361444b10bea45c6
Anwesh43 <anweshthecool0@gmail.com>
2020-09-10 02:15:35 +0530

    adding BiRightAngledTriView class

--- birightangledtriview/src/main/java/com/anwesh/uiprojects/birightangledtriview/BiRightAngledTriView.kt
+++ birightangledtriview/src/main/java/com/anwesh/uiprojects/birightangledtriview/BiRightAngledTriView.kt
@@ -67,3 +67,19 @@ fun Canvas.drawBRATNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBiRightAngleTri(scale, w, h, paint)
 }
+
+class BiRightAngledTriView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
