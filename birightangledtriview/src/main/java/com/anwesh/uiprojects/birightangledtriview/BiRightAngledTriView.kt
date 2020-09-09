ec145c835c3d38c9d5f1c35d547a27fad2e76394
Anwesh43 <anweshthecool0@gmail.com>
2020-09-10 02:05:48 +0530

    adding constants and libraries

--- birightangledtriview/src/main/java/com/anwesh/uiprojects/birightangledtriview/BiRightAngledTriView.kt
+++ birightangledtriview/src/main/java/com/anwesh/uiprojects/birightangledtriview/BiRightAngledTriView.kt
@@ -3,3 +3,26 @@ package com.anwesh.uiprojects.birightangledtriview
 /**
  * Created by anweshmishra on 10/09/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val colors : Array<Int> = arrayOf(
+        "#F44336",
+        "#00BCD4",
+        "#4CAF50",
+        "#009688",
+        "#2196F3"
+).map({Color.parseColor(it)}).toTypedArray()
+val parts : Int = 5
+val scGap : Float = 0.02f / parts
+val strokeFactor : Int = 90
+val sizeFactor : Float = 5.9f
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
+val rot : Float = 90f
